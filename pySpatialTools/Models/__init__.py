cec848125ee7eb1b891da83fcfbe398df74a700f
tgquintela <tgquintela@github.com>
2015-07-20 01:14:00 +0200

    Debug imports.

--- pySpatialTools/Models/__init__.py
+++ pySpatialTools/Models/__init__.py
@@ -6,6 +6,6 @@ Package which contains the classes needed to compute matrix descritptors or
 spatial correlation matrices.
 """
 
-from descritptor_models import DescriptorModel
+from descriptor_models import DescriptorModel
 from model_process import ModelProcess
 from pjensen import Pjensen
