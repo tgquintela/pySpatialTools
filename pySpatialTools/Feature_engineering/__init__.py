4c505bd7b5f7933d274d0a2aeebcbfae89c224f4
tgquintela <tgquintela@github.com>
2015-07-28 18:09:20 +0200

    Initial commit for feature engineering.

---
+++ pySpatialTools/Feature_engineering/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+Feature Engineering
+===================
+Package for select the most proper features in order to represent the points
+we want to use for predicting or inferring some measures from our spatial data.
+
+"""
