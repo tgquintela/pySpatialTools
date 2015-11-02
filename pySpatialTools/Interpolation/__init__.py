85bdc086084e3281df3a53ef777e7186561421b2
antonio <tgq.spm@gmail.com>
2015-11-02 17:04:24 +0100

    Density assignation in Interpolation.

--- pySpatialTools/Interpolation/__init__.py
+++ pySpatialTools/Interpolation/__init__.py
@@ -4,5 +4,14 @@ Interpolation
 =============
 Module which contains the functions to spatially interpolate features.
 
+TODO
+----
+- Join both ways into 1.
+
 """
 
+## Interpolation
+from general_interpolation import general_interpolate
+
+## Density assignation
+from density_assignation import general_density_assignation
