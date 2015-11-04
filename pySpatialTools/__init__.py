bbede943cebb4be2e706a8eba5bfa6c4bba87a7f
tgquintela <tgquintela@github.com>
2015-11-04 19:59:26 +0100

    Preparing module for optimal use.

--- pySpatialTools/__init__.py
+++ pySpatialTools/__init__.py
@@ -1,2 +1,19 @@
 
 from version import *
+
+'''
+import Correlation_Models
+import Descriptor_Models
+import Feature_engineering
+import Geo_tools
+import Interpolation
+import IO
+import Preprocess
+import Recommender
+import Retrieve
+import Sampling
+import Simulations
+import Spatial_Relations
+import Tests
+import utils
+'''
