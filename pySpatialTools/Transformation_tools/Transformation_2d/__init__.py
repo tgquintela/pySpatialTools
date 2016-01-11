a3c6b68032c56a44db3ecd47d4abd34db0dbafb0
antonio <tgq.spm@gmail.com>
2016-01-11 23:16:22 +0100

    New structure for spatial transformations.

---
+++ pySpatialTools/Transformation_tools/Transformation_2d/__init__.py
@@ -0,0 +1,18 @@
+
+"""
+Geo_tools
+=========
+Package to deal with geodata.
+
+
+TODO
+----
+geo_retrieve?
+pyproj?
+fiona?
+
+"""
+
+from geo_transformations import general_projection, radians2degrees,\
+    degrees2radians
+from geo_filters import check_in_square_area
