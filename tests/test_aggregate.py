6d7e110c4ebfec3de8451f19a4c99dff642f28d0
antonio <tgq.spm@gmail.com>
2016-01-26 23:13:42 +0100

    Changes in general tests.

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -29,3 +29,54 @@ for i in range(1, 11):
 
 res1, res2 = agg.retrieve_aggregation(df, reindices)
 
+
+
+
+def test():
+    ## Artificial distribution in space
+    locs = np.random.random((n, 2))
+    locs2 = np.array((locs[:, 0]*np.cos(locs[:, 1]*2*np.pi),
+                      locs[:, 0]*np.sin(locs[:, 1]*np.pi*2))).T
+
+    ## Test distributions
+    #fig1 = plt.plot(locs[:,0], locs[:, 1], '.')
+    #fig2 = plt.plot(locs2[:,0], locs2[:, 1], '.')
+
+    ## Discretization
+    ngx, ngy = 100, 100
+    disc0 = np.random.randint(0, 10, n)
+    disc1 = GridSpatialDisc((ngx, ngy), xlim=(0, 1), ylim=(0, 1))
+    disc2 = GridSpatialDisc((ngx, ngy), xlim=(-1, 1), ylim=(-1, 1))
+
+    n = 10000
+    locs = np.random.random((n, 2))*100
+    feat_arr0 = np.random.randint(0, 20, (n, 1))
+    feat_arr1 = np.random.random((n, 10))
+    reindices = np.arange(n).reshape((n, 1))
+
+    avgdesc = AvgDescriptor(feat_arr1)
+    countdesc = Countdescriptor(feat_arr0)
+
+    sp_descriptor = (disc0, locs, KRetriever, 5, Countdescriptor)
+    sp_descriptor = create_sp_descriptor_points_regs(sp_descriptor, regions_id, elements_i)
+    sp_descriptor.reindices = np.arange(locs.shape[0]).reshape((locs.shape[0], 1))
+    net = sp_descriptor.compute_net()[:, :, 0]
+    countdesc.compute_aggdescriptors(discretizor, regionretriever, locs)
+
+    sp_descriptor = (disc1, locs, KRetriever, 5, Countdescriptor)
+    sp_descriptor = create_sp_descriptor_points_regs(sp_descriptor, regions_id, elements_i)
+    sp_descriptor.reindices = np.arange(locs.shape[0]).reshape((locs.shape[0], 1))
+    net = sp_descriptor.compute_net()[:, :, 0]
+    countdesc.compute_aggdescriptors(discretizor, regionretriever, locs)
+
+    sp_descriptor = (disc2, locs, KRetriever, 5, Countdescriptor)
+    sp_descriptor = create_sp_descriptor_points_regs(sp_descriptor, regions_id, elements_i)
+    sp_descriptor.reindices = np.arange(locs.shape[0]).reshape((locs.shape[0], 1))
+    net = sp_descriptor.compute_net()[:, :, 0]
+    countdesc.compute_aggdescriptors(discretizor, regionretriever, locs)
+
+
+    avgdesc = AvgDescriptor(feat_arr1)
+    countdesc = Countdescriptor(feat_arr0)
+
+    avgdesc.compute_aggdescriptors(discretizor, regionretriever, locs)
--- tests/test_geotools.py
+++ tests/test_geotools.py
@@ -5,10 +5,10 @@ from pySpatialTools.Geo_tools import general_projection, check_in_square_area
 import numpy as np
 
 ## Check spain
+def test():
+    coord = np.array([[-19., 26.], [-18., 26.], [-19., 28.], [-18., 28.]])
 
-coord = np.array([[-19., 26.], [-18., 26.], [-19., 28.], [-18., 28.]])
+    lim_points = np.array([[-18.25, 4.5], [27.75, 44]])
+    logi = check_in_square_area(coord, lim_points)
 
-lim_points = np.array([[-18.25, 4.5], [27.75, 44]])
-logi = check_in_square_area(coord, lim_points)
-
-coord2 = general_projection(coord, None, 'ellipsoidal', inverse=False, radians=False)
+    coord2 = general_projection(coord, None, 'ellipsoidal', inverse=False, radians=False)
