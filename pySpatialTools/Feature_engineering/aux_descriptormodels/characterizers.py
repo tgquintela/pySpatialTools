ffc3e17ea0a2f32b807c680af7e84c98c4f0388d
antonio <tgq.spm@gmail.com>
2016-02-02 16:44:23 +0100

    New characterizor creator function for weighting average with kernel functions computed in interpolation utils.

--- pySpatialTools/Feature_engineering/aux_descriptormodels/characterizers.py
+++ pySpatialTools/Feature_engineering/aux_descriptormodels/characterizers.py
@@ -20,6 +20,8 @@ OUTPUTS:
 
 import numpy as np
 from collections import Counter
+from pySpatialTools.Feature_engineering.Interpolation_utils.\
+    density_assignation import from_distance_to_weights, compute_measure_i
 
 
 def characterizer_1sh_counter(pointfeats, point_pos):
@@ -51,3 +53,34 @@ def characterizer_average(pointfeats, point_pos):
     pointfeats = np.array(pointfeats)
     descriptors = np.mean(pointfeats, axis=0)
     return descriptors
+
+
+def create_weighted_function(f_weighs, params_w, f_dens, params_d):
+    """Functions which acts in order to create a weighted function. You have to
+    give the needed parameters and it returns to you a function.
+
+    Parameters
+    ----------
+    f_weighs: functions, str
+        function of weighs assignation. It transforms the distance to weights.
+    params_w: dict
+        parameters needed to apply f_weighs.
+    f_dens: function, set_scale_surgauss
+        function of density assignation.
+    params_d: dict
+        parameters needed to apply f_dens.
+
+    Returns
+    -------
+    f: function
+        a function which has as inputs the dists and values of the neighs
+        points.
+
+    """
+
+    def f(values, dists):
+        weights = from_distance_to_weights(dists, f_weighs, params_w)
+        M = compute_measure_i(weights, values, f_dens, params_d)
+        return M
+
+    return f
