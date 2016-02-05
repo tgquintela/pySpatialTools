30769615d1a1fe9882ca7f4e45a20a44e2505312
antonio <tgq.spm@gmail.com>
2016-02-05 12:58:32 +0100

    Preprocess locations update.

--- pySpatialTools/Preprocess/__init__.py
+++ pySpatialTools/Preprocess/__init__.py
@@ -1,3 +1,13 @@
 
+"""
+Preprocess
+==========
+Collection of preprocess functions and methods.
+
+"""
 
 from aggregation import Aggregator
+
+## Preprocess locations
+from locations_preprocess import remove_unknown_locations,\
+    jitter_group_imputation
---
+++ pySpatialTools/Preprocess/locations_preprocess.py
@@ -0,0 +1,33 @@
+
+"""
+Locations preprocess
+--------------------
+Module which groups all the methods to preprocess locations.
+"""
+
+import numpy as np
+
+
+def remove_unknown_locations(locations, logi):
+    "Remove unknown locations."
+    return locations[logi]
+
+
+def jitter_group_imputation(locations, logi, groups):
+    "Jitter the average locations of the group."
+    if np.all(logi):
+        return locations
+    u_groups, new_locations = np.unique(groups), np.zeros(locations.shape)
+    new_locations[logi] = locations[logi]
+    for g in u_groups:
+        ## Select group
+        logi_unk_g = np.logical_and(np.logical_not(logi), groups == g)
+        locs_g = locations[np.logical_and(logi, groups == g)]
+        ## Statistics of the group
+        loc_g, std_g = np.mean(locs_g, axis=0), np.std(locs_g, axis=0)
+        ## Build new coordinates
+        jitter_g = np.random.random((np.sum(logi_unk_g), locations.shape[1]))
+        new_coordinates = np.multiply(std_g, jitter_g) + loc_g
+        ## Imputation
+        new_locations[logi_unk_g] = new_coordinates
+    return new_locations
