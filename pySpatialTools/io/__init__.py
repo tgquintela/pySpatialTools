7fa1e0cb05ee2ca071c8dda8e631bd9182de5a3c
antonio <tgq.spm@gmail.com>
2016-02-28 01:03:02 +0100

    Changes in IO.

--- pySpatialTools/io/__init__.py
+++ pySpatialTools/io/__init__.py
@@ -6,6 +6,3 @@ Package to deal with importing or exporting data.
 It also is able to create useful data.
 """
 
-
-from data_creation_utils import create_syntetic_data, create_reindices,\
-    format_info_ret, format_cond_agg
---
+++ pySpatialTools/io/io_images.py
@@ -0,0 +1,28 @@
+
+"""
+io images
+---------
+Module to format images to be used by this framework.
+
+"""
+
+import numpy as np
+from itertools import product
+
+
+def create_locs_features_from_image(image):
+    """Create locs and features from image."""
+    sh = image.shape
+    if len(sh) == 2:
+        image = image.reshape((sh[0], sh[1], 1))
+
+    map2indices = lambda x, y: x + y*sh[0]
+
+    n = np.prod(sh[:2])
+    locs = np.zeros((n, 2)).astype(int)
+    feats = np.zeros((n, image.shape[2])).astype(int)
+    for p in product(xrange(sh[0]), xrange(sh[1])):
+        i = map2indices(p[0], p[1])
+        locs[i] = np.array([p[0], p[1]])
+        feats[i] = image[p[0], p[1], :]
+    return locs, feats
