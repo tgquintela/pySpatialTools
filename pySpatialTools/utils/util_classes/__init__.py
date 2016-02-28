7f1542bc52438e6c9796e776603553d7f5a9df7f
antonio <tgq.spm@gmail.com>
2016-02-28 03:22:08 +0100

    Debug in importing deleted module.

--- pySpatialTools/utils/util_classes/__init__.py
+++ pySpatialTools/utils/util_classes/__init__.py
@@ -9,5 +9,4 @@ Classes which represent data types useful for the package pySpatialTools.
 from spdesc_mapper import Sp_DescriptorMapper
 from spatialelements import SpatialElementsCollection, Locations
 from Membership import Membership
-from general_mapper import General1_1Mapper
 from mapper_vals_i import Map_Vals_i, create_mapper_vals_i
